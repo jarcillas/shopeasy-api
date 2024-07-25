ce251b072c0a76544c8c81066bfa4efebe8b89ff
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-07-25 18:30:47 +0800

    Update response to JSON

--- api/index.js
+++ api/index.js
@@ -3,7 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Express on Vercel');
+  res.json({
+    grocerylist: [
+      { name: 'Coke', amount: 2, unit: 'L', note: 'For the party' },
+      {
+        name: 'Purefoods Corned Beef 80g',
+        amount: 2,
+        unit: 'cans',
+        note: 'For breakfast',
+      },
+    ],
+  });
 });
 
 app.listen(5000, () => {
