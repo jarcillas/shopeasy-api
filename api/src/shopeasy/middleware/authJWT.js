6679e0b790538344c5044c4e3806192c5b4aa350
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-08-26 20:31:53 +0800

    add middleware for JWT authentication

---
+++ api/src/shopeasy/middleware/authJWT.js
@@ -0,0 +1,88 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/auth.config');
+const { User } = require('../db');
+
+const verifyToken = (req, res, next) => {
+  let token = req.headers['x-access-token'];
+
+  if (!token) {
+    return res.status(430).send({
+      message: 'No token provided!',
+    });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: 'Unauthorized!',
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+const isAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then((user) => {
+    user.getRoles().then((roles) => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'admin') {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: 'Admin role required!',
+      });
+      return;
+    });
+  });
+};
+
+const isModerator = (req, res, next) => {
+  User.findByPk(req.userId).then((user) => {
+    user.getRoles().then((roles) => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'moderator') {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: 'Moderator role required!',
+      });
+      return;
+    });
+  });
+};
+
+const isModeratorOrAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then((user) => {
+    user.getRoles().then((roles) => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'moderator') {
+          next();
+          return;
+        }
+
+        if (roles[i].name === 'admin') {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: 'Moderator or Admin role required!',
+      });
+    });
+  });
+};
+
+module.exports = {
+  verifyToken,
+  isAdmin,
+  isModerator,
+  isModeratorOrAdmin,
+};
